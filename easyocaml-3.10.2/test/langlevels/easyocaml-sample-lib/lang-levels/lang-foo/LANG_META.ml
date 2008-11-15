3de808f42b7ebdbfaf816607de310105f250f27f
Benedikt Becker <benozol@users.noreply.github.com>
2008-11-15 11:39:12 +0000

    Script to compile all language levels at once

--- easyocaml-3.10.2/test/langlevels/easyocaml-sample-lib/lang-levels/lang-foo/LANG_META.ml
+++ easyocaml-3.10.2/test/langlevels/easyocaml-sample-lib/lang-levels/lang-foo/LANG_META.ml
@@ -6,11 +6,11 @@ open EzyLangLevel
 
 let _ =
   let match_pf =
-    let full_pf = all_pattern_features true in
+    let full_pf = all_pattern_feats true in
     { full_pf with p_nested = false } in
   let prf = 
-    let full_ef = all_expr_features true in {
-      pr_expr_features = { full_ef with e_try = None; e_match = Some match_pf; e_raise = false } ;
-      pr_struct_features = all_struct_features true ;
+    let full_ef = all_expr_feats true in {
+      pr_expr_feats = { full_ef with e_try = None; e_match = Some match_pf; e_raise = false } ;
+      pr_struct_feats = all_struct_feats true ;
     } in
   configure prf [("M1", true); ("M2", false); ("List", true); ("String", false)] ["m1.cmo"; "m2.cmo"]
