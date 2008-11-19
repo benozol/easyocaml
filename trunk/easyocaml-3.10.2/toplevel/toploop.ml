aa7cdf69306211f5a562fbeb3b4b3ecc26e897e8
benusb <benusb@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2008-11-19 10:32:01 +0000

    * Cleanup of program annotations of errors (invisible Pre(AnnotatedError|Fatal) is wrapped by wrap_exception_with_program  * wrap_exception_with_program in toploop

--- trunk/easyocaml-3.10.2/toplevel/toploop.ml
+++ trunk/easyocaml-3.10.2/toplevel/toploop.ml
@@ -221,7 +221,8 @@ let execute_phrase print_outcome ppf phr =
       let str, sg, newenv =
         match (EzySetup.setup ()).EzySetup.features with
           | Some fs ->
-              snd (EzyTyping.type_and_compare_top_phrase fs oldenv sstr)
+              EzyErrors.wrap_exception_with_program (lazy "")
+                (fun () -> snd (EzyTyping.type_and_compare_top_phrase fs oldenv sstr))
           | None ->
               let _ = Unused_var.warn ppf sstr in
               Typecore.reset_delayed_checks ();
