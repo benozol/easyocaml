fb05891c07727a029e9eb2d1a0634887050ea638
benusb <benusb@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2009-01-27 23:43:40 +0000

    pureley functional, small API lang level: lang-fun

--- trunk/ocaml-3.11.0+eased/easyocaml-additional/easyocaml-sample-lib/lang-levels/lang-minimal/LANG_META.ml
+++ trunk/ocaml-3.11.0+eased/easyocaml-additional/easyocaml-sample-lib/lang-levels/lang-minimal/LANG_META.ml
@@ -1,26 +1,36 @@
 open EzyLangLevel
 
 let _ =
-  let min_patt = { (all_pattern_feats false) with p_var = true } in
-  let ff = 
-    { f_fun = false; f_pattern = min_patt } in
-  let let_feats = { (all_let_feats false) with l_pattern = min_patt } in
-  let ef =
-    { (all_expr_feats false) with
-          e_match = Some { (all_pattern_feats false) with p_var = true; p_wildcard = true; p_constant = true } ; 
-          e_constant = true; 
-          e_let_in = Some let_feats;
-          e_simple_var = true; 
-          e_function = Some ff;
-          e_list = true;
-          e_tuple = true;
-          e_constructor = true;
-          e_if_then_else = true; } in
-  let sf =
-    { (all_struct_feats false) with
-          s_eval_expr = true;
-          s_annot_optional = true;
-          s_let = Some let_feats } in
-  let prf = 
-    { pr_expr_feats = ef; pr_struct_feats = sf } in
-  configure prf ["List", true; "Pervasives", true] []
+  let min_patt_feats =
+    { (all_pattern_feats false) with
+        p_var = true } in
+  let match_patt_feats =
+    { (all_pattern_feats false) with
+        p_wildcard = true;
+        p_constant = true;
+        p_list = true; } in
+  let let_feats =
+    { l_pattern = min_patt_feats;
+      l_args = false;
+      l_and = true; } in
+  let let_rec_feats =
+    { lr_and = true;
+      lr_args = false } in
+  let func_feats = 
+    { f_fun = false; f_pattern = min_patt_feats } in
+  let program_feats = 
+    { pr_expr_feats =
+        { (all_expr_feats false) with
+            e_constant = true;
+            e_function = Some func_feats;
+            e_if_then_else = true;
+            e_list = true;
+            e_match = Some match_patt_feats;
+            e_simple_var = true; };
+      pr_struct_feats = 
+        { (all_struct_feats false) with
+            s_annot_optional = false;
+            s_let = Some let_feats;
+            s_let_rec = Some let_rec_feats }
+    } in
+  configure program_feats ["Mypervasives", true; "Mylist", true] ["mylist.cmo"; "mypervasives.cmo"]
