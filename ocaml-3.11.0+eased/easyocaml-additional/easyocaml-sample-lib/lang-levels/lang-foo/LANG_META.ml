54502c299358c605e58fb0f2b4427058af853e45
Benedikt Becker <benozol@users.noreply.github.com>
2009-01-27 23:43:40 +0000

    pureley functional, small API lang level: lang-fun

--- ocaml-3.11.0+eased/easyocaml-additional/easyocaml-sample-lib/lang-levels/lang-foo/LANG_META.ml
+++ ocaml-3.11.0+eased/easyocaml-additional/easyocaml-sample-lib/lang-levels/lang-foo/LANG_META.ml
@@ -1,16 +1,36 @@
 open EzyLangLevel
 
-(* 1/ match expressions are allowed but the patterns are not allowed to be nested.
- * 2/ exception handling is disallowd
- *)
-
 let _ =
+  let min_patt =
+    { (all_pattern_feats false) with
+        p_var = true } in
   let match_pf =
-    let full_pf = all_pattern_feats true in
-    { full_pf with p_nested = false } in
-  let prf = 
-    let full_ef = all_expr_feats true in {
-      pr_expr_feats = { full_ef with e_try = None; e_match = Some match_pf; e_raise = false } ;
-      pr_struct_feats = all_struct_feats true ;
-    } in
-  configure prf [("M1", true); ("M2", false); ("List", true); ("String", false)] ["m1.cmo"; "m2.cmo"]
+    { (all_pattern_feats false) with
+        p_wildcard = true;
+        p_constant = true;
+        p_list = true; } in
+  let let_feats =
+    { l_pattern = min_patt;
+      l_args = false;
+      l_and = true; } in
+  let let_rec_feats =
+    { lr_and = true;
+      lr_args = false } in
+  let func_feats = 
+    { f_fun = false; f_pattern = min_patt } in
+  let program_feats = 
+    { pr_expr_feats =
+        { (all_expr_feats false) with
+            e_constant = true;
+            e_function = func_feats;
+            e_if_then_else = true;
+            e_list = true;
+            e_match = true;
+            e_simple_var = true; };
+      pr_struct_feats = 
+        { (all_struct_feats false) with
+            s_annot_optional = false;
+            s_let = Some let_feats;
+            s_let_rec = Some let_rec_feats;
+            s_semisemi_optional = true } in
+  configure program_feats ["Mypervasives", true; "Mylist", true]
---
+++ ocaml-3.11.0+eased/easyocaml-additional/easyocaml-sample-lib/lang-levels/lang-fun/LANG_META.ml
@@ -0,0 +1,24 @@
+open EzyLangLevel
+
+let _ =
+  let pr_f = {
+    pr_expr_feats = {
+      (all_expr_feats true) with
+        e_reference_update = false;     (* forbid [x := e] *)
+        e_record_field_update = false;  (* forbid [x.f <- e] *)
+        e_if_then = false;              (* forbid [if e then f] *)
+        e_sequence = false;             (* forbid [e;f] *)
+    };
+    pr_struct_feats = {
+      (all_struct_feats true) with
+          s_annot_optional = false;   (* make annotations for global variables mandatory *)
+          s_type = Some {
+            (all_type_feats true) with
+                t_record = Some false (* forbid declaration of mutable record fields *)
+          }
+    }
+  } in
+  configure pr_f [
+    "Mylist", true;
+    "Mypervasives", true;
+  ] ["mylist.cmo"; "mypervasives.cmo"]
